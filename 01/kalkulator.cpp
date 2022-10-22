94b2c799f08c6432dcdd12c05e1535ef603370c0
Stefan Karczewski <s27459@pjwstk.edu.pl>
2022-10-22 13:07:49 +0200

    Make the calculator ask for input indefinitely

--- 01/kalkulator.cpp
+++ 01/kalkulator.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 
 int main() {
@@ -5,42 +6,50 @@ int main() {
               << "________________\n\n";
 
     double a, b;
-    std::cout << "Podaj pierwsza liczbe: ";
-    std::cin >> a;
-    std::cout << "Podaj druga liczbe: ";
-    std::cin >> b;
-
-    std::cout << "\nWybierz dzialanie:\n"
-              << "\t1. Dodawanie\n"
-              << "\t2. Odejmowanie\n"
-              << "\t3. Mnozenie\n"
-              << "\t4. Dzielenie\n"
-              << "\t5. Reszta z dzielenia\n"
-              << "\t0. Wyjscie\n";
     int action;
-    std::cout << "Twoj wybor: ";
-    std::cin >> action;
 
-    switch (action) {
-        case 1:
-            std::cout << a << '+' << b << '=' << a + b << '\n';
+    while (true) {
+        std::cout << "Podaj pierwsza liczbe: ";
+        std::cin >> a;
+        std::cout << "Podaj druga liczbe: ";
+        std::cin >> b;
+
+        std::cout << "\nWybierz dzialanie:\n"
+                  << "\t1. Dodawanie\n"
+                  << "\t2. Odejmowanie\n"
+                  << "\t3. Mnozenie\n"
+                  << "\t4. Dzielenie\n"
+                  << "\t5. Reszta z dzielenia\n"
+                  << "\t0. Wyjscie\n";
+        std::cout << "Twoj wybor: ";
+        std::cin >> action;
+
+        if (action == 0) {
+            std::cout << "Na razie :)\n";
             break;
-        case 2:
-            std::cout << a << '-' << b << '=' << a - b << '\n';
-            break;
-        case 3:
-            std::cout << a << '*' << b << '=' << a * b << '\n';
-            break;
-        case 4:
-            std::cout << a << '/' << b << '=' << a / b << '\n';
-            break;
-        case 5:
-            std::cout << a << '%' << b << '=' << (int)a % (int)b << '\n';
-            break;
-        case 0:
-            return 0;
-        default:
-            std::cout << "Podano złą opcję\n";
-            return -1;
+        }
+
+        switch (action) {
+            case 1:
+                std::cout << a << '+' << b << '=' << a + b << '\n';
+                break;
+            case 2:
+                std::cout << a << '-' << b << '=' << a - b << '\n';
+                break;
+            case 3:
+                std::cout << a << '*' << b << '=' << a * b << '\n';
+                break;
+            case 4:
+                std::cout << a << '/' << b << '=' << a / b << '\n';
+                break;
+            case 5:
+                std::cout << a << '%' << b << '=' << fmod(a, b) << '\n';
+                break;
+            default:
+                std::cout << "Podano nieistniejącą opcję\n";
+                continue;
+        }
+
+        std::cout << '\n';
     }
 }
